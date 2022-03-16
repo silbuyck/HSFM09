4ed6dc07f17ee57d311682661454b5015e348dd2
silbuyck <76997305+silbuyck@users.noreply.github.com>
2022-03-16 15:03:50 +0100

    leaflet met eigen laag

--- js/mapleaf.js
+++ js/mapleaf.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([52.377956, 4.897070], 13);
 			L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 				maxZoom: 18,
@@ -7,3 +7,20 @@ var map = L.map('map').setView([51.505, -0.09], 13);
 				zoomOffset: -1,
 				accessToken: 'pk.eyJ1IjoiMzAyOTI5MyIsImEiOiJjbDA2dnQweDUwMDl0M2Jwcnk0N2g2aHAyIn0.ZvZ0jDA_yTce_ghJM8Glew'
 			}).addTo(map);
+			
+			// ADD WFS layer
+		//const cbs = L.tileLayer.wms('https://geodata.nationaalgeoregister.nl/ahn3/wms', {
+			//'layers': 'ahn3_5m_dtm',
+			//'styles': 'ahn3:ahn3_5m_detail',
+			//'srs': 'EPSG:28992',
+			//'format': 'image/png',
+			//'transparent': true
+		//}).addTo(map);
+		
+		const geoserverWFSBuurt = L.tileLayer.wms('http://localhost:8001/geoserver/ows', {
+			'layers': 'buurt_2019_v1',
+			'styles': 'polygon',
+			'srs': 'EPSG:28992',
+			'format': 'image/png',
+			'transparent': true
+		}).addTo(map);
