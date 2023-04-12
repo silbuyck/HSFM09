9f552cd36a4888ec69da92ba14751720b837b04d
silbuyck <76997305+silbuyck@users.noreply.github.com>
2023-04-12 19:10:44 +0200

    Kleine stijlwijzingen

--- js/mapArcGisApi.js
+++ js/mapArcGisApi.js
@@ -8,8 +8,8 @@ require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig,M
 		
 	const view = new MapView({
         map: map,
-        center: [-118.805, 34.027], // Longitude, latitude
-        zoom: 13, // Zoom level
+        center: [117.280689, -0.846022], // Longitude, latitude
+        zoom: 3, // Zoom level
         container: "mapArcGisApi" // Div element
     });
 
