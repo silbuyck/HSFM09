edefd725673eaff4ce875b2d011a23bb0a702653
silbuyck <76997305+silbuyck@users.noreply.github.com>
2023-04-12 16:03:57 +0200

    Update mapArcGisApi.js

--- js/mapArcGisApi.js
+++ js/mapArcGisApi.js
@@ -12,19 +12,4 @@ require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig,M
         zoom: 3, // Zoom level
         container: "mapArcGisApi" // Div element
     });
-		 
-  const geoJson = L.geoJson().addTo(map);
-
-    function zoomOnClick() {
-      map.setView( [117.280689, -0.846022], 3)
-
-      fetch('https://github.com/georgique/world-geojson/blob/dc7d0ff0b36b3694df9a153566cdc964a183d213/countries/indonesia.json')
-        .then(response => response.json())
-        .then(data => {
-          const wkt = data.response.docs[0].geometrie_ll
-		  
-          const geojson = Terraformer.wktToGeoJSON(wkt)
-          console.log(geojson)
-          geoJsonLayer.addData(geojson)
-        })
     }
