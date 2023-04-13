cda21ddea48b7fb9c4b505edb40b1e77fdffc640
silbuyck <76997305+silbuyck@users.noreply.github.com>
2023-04-13 10:52:42 +0200

    Openlayers werkende :)

--- js/mapOpenLayer.js
+++ js/mapOpenLayer.js
@@ -1,20 +1,25 @@
-const key = 'LkdVK31IBQxIxw3CTSwU';
-const source = new ol.source.TileJSON({
-  url: `https://api.maptiler.com/maps/streets-v2/tiles.json?key=${key}`,
-  tileSize: 512,
-  crossOrigin: 'anonymous'
-});
+const key = 'ra5kynhRPGd1fkCh3rvE';
+      const source = new ol.source.TileJSON({
+        url: `https://api.maptiler.com/maps/streets-v2/tiles.json?key=${key}`,
+        tileSize: 512,
+        crossOrigin: 'anonymous'
+      });
 
-const map = new ol.Map({
-  layers: [
-    new ol.layer.Tile({
-      source: source
-    })
-  ],
-  target: 'map',
-  view: new ol.View({
-    constrainResolution: true,
-    center: ol.proj.fromLonLat([16.62662018, 49.2125578]),
-    zoom: 14
-  })
-});
+      const attribution = new ol.control.Attribution({
+        collapsible: false,
+      });
+      
+      const map = new ol.Map({
+        layers: [
+          new ol.layer.Tile({
+            source: source
+          })
+        ],
+        controls: ol.control.defaults.defaults({attribution: false}).extend([attribution]),
+        target: 'mapOL',
+        view: new ol.View({
+          constrainResolution: true,
+          center: ol.proj.fromLonLat([16.62662018, 49.2125578]),
+          zoom: 14
+        })
+      });
